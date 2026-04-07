cc27aa96f395738d2dd3dcd9a1fd55a84b500739
nadi <nadiaslcowins@gmail.com>
2026-04-06 22:46:06 -0400

    Created TagExtractorFrame, added Gui and created skeleton for rest of methods

--- src/TagExtractorFrame.java
+++ src/TagExtractorFrame.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Stream;
 
-public class TagExtractorFrame {
+public class TagExtractorFrame extends JFrame {
+    Set<String> stopWords = new TreeSet<>(); //cant have duplicates, stores noise words, sorts automatically
+    Map<String, Integer> tagMap = new TreeMap<>();// stores key and value, sorts automatically
+
+    //gui
+    JTextArea displayArea;
+    JLabel fileNameLbl;
+    Path bookPath;
+
+    public TagExtractorFrame() {
+
+    }
+
+    public void loadStopWords () {
+
+    }
+
+    private void extractTags() {
+
+    }
+
+    private void saveTags() {
+
+    }
 }
